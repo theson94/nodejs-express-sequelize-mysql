5fc37ce025fcac028c9c1f47d082dbbd11357c55
theson94 <theson94@gmail.com>
2022-08-16 16:10:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:8082"
 };
 app.use(cors(corsOptions));
 // parse requests of content-type - application/json
